b8ffab6cca0c6aa6d79753979cc93e661c9e4388
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-02-23 19:56:00 +0100

    Initial Svelte + typescript + routing setup

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import {Router, Route, Link} from 'svelte-routing';
+  import Basics from './components/Introduction/Basics.svelte';
+
+  export let url = '';
+</script>
+
+<main>
+  <h1>Svelte tutorial and sandbox</h1>
+  <Router url={url}>
+    <nav>
+      <Link to="introduction/basics">Basics</Link>
+    </nav>
+    <Route path="introduction/basics" component="{Basics}"/>
+  </Router>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/components/Introduction/Basics.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+</script>
+
+<div>
+  Basics works!
+</div>
+
+<style>
+
+</style>
