83c8533971deae72b9741300f3fce8a07cafaeb9
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-02-24 19:38:14 +0100

    Delete test component

--- src/components/Introduction/Introduction.svelte
+++ src/components/Introduction/Introduction.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
-	import Test from './Test.svelte';
-
 	let name = 'test';
-	const asd = 'testaaa';
 </script>
 
 <section>
 	Introduction
 	{name}
-	<Test asd="{asd}" />
 </section>
 
 <style>
--- src/components/Introduction/Test.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  export let asd: string;
-
-</script>
-
-<p>
-  {asd}
-</p>
