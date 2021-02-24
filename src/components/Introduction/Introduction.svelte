d2eca26d8e51c2760c42c7f7232e80dca64ee928
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-02-24 20:01:56 +0100

    Introduction tutorial completed

--- src/components/Introduction/Introduction.svelte
+++ src/components/Introduction/Introduction.svelte
@@ -1,11 +1,37 @@
 <script lang="ts">
-	let name = 'test';
+	import Nested from './Nested.svelte';
+
+	const name = 'testName';
+	const src = 'https://fakeimg.pl/200x100/?retina=1&text=こんにちは&font=noto';
+	const htmlEntities = '&amp; &copy;';
 </script>
 
 <section>
 	Introduction
-	{name}
+	<div>
+		<p>Uppercase variable {name.toUpperCase()}</p>
+		<p>Concatenated variable {'super' + name}</p>
+	</div>
+	<div>
+		<img {src} alt="test" />
+	</div>
+	<div>
+		<Nested />
+	</div>
+	<div class="special">
+		{@html htmlEntities}
+	</div>
 </section>
 
 <style>
+	section {
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
+	}
+
+	.special {
+		color: #333333;
+		font-weight: bolder;
+	}
 </style>
---
+++ src/components/Introduction/Nested.svelte
@@ -0,0 +1,8 @@
+<p>I am nested component</p>
+<div>Does it work?</div>
+
+<style>
+	* {
+		color: red;
+	}
+</style>
