039a2a657fa1ebd5a652ab6ad043b587f77cc3da
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-02-24 20:15:29 +0100

    Reactivity tutorial in progress

--- src/components/Reactivity/Reactivity.svelte
+++ src/components/Reactivity/Reactivity.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
-
+	let count = 5;
 </script>
 
 <section>
-  Reactivity
+	Reactivity
+	<div>
+		<button
+			on:click={() => {
+				count *= 2;
+			}}>
+			Double the {count}
+		</button>
+	</div>
 </section>
 
 <style>
-
+	section {
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
+	}
 </style>
