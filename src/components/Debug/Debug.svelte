43fc1a3027bd0268564c58f4e43699aafb3e9bb3
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-03-08 21:19:10 +0100

    Add back to home to debug

--- src/components/Debug/Debug.svelte
+++ src/components/Debug/Debug.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+	import BackToHome from '../BackToHome/BackToHome.svelte';
+
 	let value = 5;
 </script>
 
 <section>
+	<BackToHome />
 	<input type="number" bind:value />
 	{@debug value}
 </section>
