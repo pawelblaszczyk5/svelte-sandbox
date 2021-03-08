dcbc42dc9b5c60ca90e70df454d967bee6bdf26c
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-03-08 21:11:17 +0100

    ModuleContext chapter completed

--- src/components/ModuleContext/ModuleContext.svelte
+++ src/components/ModuleContext/ModuleContext.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
+	import TestComponent, {resetCounter} from './TestComponent.svelte';
+	let currentTestComponentCount = 1;
+	$: arr = new Array(currentTestComponentCount);
 </script>
 
 <section>
 	<p>ModuleContext works!</p>
+	<button on:click={() => currentTestComponentCount++}> Add new TestComponent </button>
+	<button on:click={resetCounter}>Reset counter</button>
+	{#each arr as test}
+		<TestComponent />
+	{/each}
 </section>
 
 <style>
---
+++ src/components/ModuleContext/TestComponent.svelte
@@ -0,0 +1,15 @@
+<script context="module" lang="ts">
+	let count = 0;
+	export const resetCounter = (): void => {
+		count = 0;
+	};
+</script>
+
+<script lang="ts">
+	count++;
+</script>
+
+<p>I was created {count} {count > 1 ? 'times' : 'time'}!</p>
+
+<style>
+</style>
