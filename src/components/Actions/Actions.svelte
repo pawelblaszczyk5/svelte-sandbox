29a0fdd9fabe35b8173cf19f8b207307ca6529e9
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-03-08 19:57:07 +0100

    Added back to home link to actions

--- src/components/Actions/Actions.svelte
+++ src/components/Actions/Actions.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import {hover} from '../../actions/hover';
+	import BackToHome from '../BackToHome/BackToHome.svelte';
 
 	let shouldShowActionParagraphs = true;
 </script>
 
 <section>
+	<BackToHome />
 	<label><input type="checkbox" bind:checked={shouldShowActionParagraphs} /> Show/Hide paragraphs</label>
 	{#if shouldShowActionParagraphs}
 		<p use:hover>Actions works!</p>
