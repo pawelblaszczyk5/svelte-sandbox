5ff9335673af7911ce732ac24fcde5a8757cc608
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-03-08 20:04:16 +0100

    Classes chapter completed

--- src/components/Classes/Classes.svelte
+++ src/components/Classes/Classes.svelte
@@ -1,7 +1,33 @@
 <script lang="ts">
+	import BackToHome from '../BackToHome/BackToHome.svelte';
+
+	let big = false;
+	const classes = ['nice', 'funny', 'peculiar'];
+	let selectedClass = 'peculiar';
 </script>
 
-<p>Classes works!</p>
+<section>
+	<BackToHome />
+	<label><input type="checkbox" bind:checked={big} /> Add class to paragraph below</label>
+	<p class:big>I am {big ? 'big' : 'small'}</p>
+	<p>Select nice class for paragraph below</p>
+	<select bind:value={selectedClass}>
+		{#each classes as className}
+			<option>{className}</option>
+		{/each}
+	</select>
+	<p class:nice={selectedClass === 'nice'}>I am paragraph</p>
+</section>
 
 <style>
+	.big {
+		font-size: 2em;
+		color: turquoise;
+	}
+
+	.nice {
+		color: pink;
+		padding: 0.5rem;
+		border: 2px solid #21ffc2;
+	}
 </style>
